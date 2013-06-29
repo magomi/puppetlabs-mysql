685848ad3348054d2cc287d432fee19cb1e5d67f
Marco Grunert <marco.grunert@intecsoft.de>
2013-06-29 12:44:05 +0200

    Fixes some backup issues
     - use the --event option to prevent stop after backup of the
       mysql database (mysqdump will stop after warnings/errors)
     - backup user needs more privileges tu backup the mysql database
       (by now all privileges are granted - TODO: checkout which
       ones are really necessary)

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -41,7 +41,7 @@ class mysql::backup (
   }
 
   database_grant { "${backupuser}@localhost":
-    privileges => [ 'Select_priv', 'Reload_priv', 'Lock_tables_priv', 'Show_view_priv' ],
+    privileges => [ 'Select_priv' , 'Insert_priv' , 'Update_priv' , 'Delete_priv' , 'Create_priv' , 'Drop_priv' , 'Reload_priv' , 'Shutdown_priv' , 'Process_priv' , 'File_priv' , 'Grant_priv' , 'References_priv' , 'Index_priv' , 'Alter_priv' , 'Show_db_priv' , 'Super_priv' , 'Create_tmp_table_priv' , 'Lock_tables_priv' , 'Execute_priv' , 'Repl_slave_priv' , 'Repl_client_priv' , 'Create_view_priv' , 'Show_view_priv' , 'Create_routine_priv' , 'Alter_routine_priv' , 'Create_user_priv' , 'Event_priv' , 'Trigger_priv' , 'Create_tablespace_priv' ],
     require    => Database_user["${backupuser}@localhost"],
   }
 
